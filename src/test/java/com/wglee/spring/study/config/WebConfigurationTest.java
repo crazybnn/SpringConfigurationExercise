d7a635f027d2ad03d5c4e86506fe077e9982d2a0
Lee Wangeun <wglee21g@gmail.com>
2015-01-23 23:31:34 +0900

    Step 04 브랜치 소스코드 정리

--- src/test/java/com/wglee/spring/study/config/WebConfigurationTest.java
+++ src/test/java/com/wglee/spring/study/config/WebConfigurationTest.java
@@ -4,7 +4,6 @@ import com.wglee.spring.study.model.Article;
 import com.wglee.spring.study.model.Tag;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
@@ -24,26 +23,6 @@ public class WebConfigurationTest {
 	@Test
 	public void getTagBean_Annotation() {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfiguration.class);
-		Article article = context.getBean("article", Article.class);
-
-		List<Tag> tags = article.getTags();
-		int size = tags.size();
-
-		assertEquals(size, 1);
-	}
-
-	@Test
-	public void getTagContainerBean_XML() throws Exception {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-		Article article = context.getBean("article", Article.class);
-
-		assertNotNull(article);
-	}
-
-	@Test
-	public void getTagBean_XML() throws Exception {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Article article = context.getBean("article", Article.class);
 
 		List<Tag> tags = article.getTags();
