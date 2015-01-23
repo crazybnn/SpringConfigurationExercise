faafded98f28c2450939061c568b75fec8fe5e69
Lee Wangeun <wglee21g@gmail.com>
2015-01-23 23:19:20 +0900

    Step 02 브랜치 정리

--- src/test/java/com/wglee/spring/study/config/WebConfigurationTest.java
+++ src/test/java/com/wglee/spring/study/config/WebConfigurationTest.java
@@ -3,7 +3,6 @@ package com.wglee.spring.study.config;
 import com.wglee.spring.study.model.Article;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -11,15 +10,6 @@ public class WebConfigurationTest {
 	@Test
 	public void getTagContainerBean_Annotation() {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfiguration.class);
-
-		Article article = context.getBean("article", Article.class);
-
-		assertNotNull(article);
-	}
-
-	@Test
-	public void getTagContainerBean_XML() throws Exception {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		Article article = context.getBean("article", Article.class);
 
